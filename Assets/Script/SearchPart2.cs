f7beb3723e04073de3b26e2a5c945596189a1493
Tore Friis <torefriis@gmail.com>
2020-07-04 17:26:31 +0200

    Highlighted hav som man skal være på for at få kunder om bord

---
+++ Assets/Script/SearchPart2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SearchPart2 : MonoBehaviour
+{
+    public Vector3 target;
+    public float customerSpeed;
+    bool hasReached = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // makes the customers go towards a certain target point
+        if(hasReached == false){
+            transform.position = Vector3.MoveTowards(transform.position, target, customerSpeed);
+        }
+        if(transform.position == target){
+            hasReached = true;
+        }
+    }
+}
