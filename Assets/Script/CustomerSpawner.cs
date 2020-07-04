46e6da9fe704df5c447089ac3d600da4b321bef4
Tore Friis <torefriis@gmail.com>
2020-07-04 11:56:00 +0200

    Første version

---
+++ Assets/Script/CustomerSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerSpawner : MonoBehaviour
+{
+    public GameObject customerPrefab;
+    public float centerPoint;
+    public float spawnRadius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnFood();
+    }
+
+    public void SpawnFood(){
+        Vector3 position = new Vector3(Random.Range(-spawnRadius/2, spawnRadius/2), 1, Random.Range(-spawnRadius/2, spawnRadius/2));
+
+        Instantiate(customerPrefab, position, Quaternion.identity);
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Transform playerTF;
+    public float forwardForce;
+    public float backwardsForce;
+    public float rotationScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Input.GetKey("w")){
+            rb.AddRelativeForce(0, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        if(Input.GetKey("s")){
+            rb.AddRelativeForce(0, 0, -backwardsForce * Time.deltaTime, ForceMode.VelocityChange);
+        } 
+        if(Input.GetKey("d")){
+            playerTF.Rotate(0f, rotationScale, 0f);
+        }
+        if(Input.GetKey("a")){
+            playerTF.Rotate(0f, -rotationScale, 0f);
+        }
+   }
+}
