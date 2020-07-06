d6771c69bd9f80a6a7c6d7d6ec0b5fd4578584b6
Tore Friis <torefriis@gmail.com>
2020-07-06 13:06:48 +0200

    Customers on boat text added!

---
+++ Assets/Script/CustomerTracker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomerTracker : MonoBehaviour
+{
+    GameObject onBoatDisplay;
+    Text onBoatDisplayText;
+    public capacityManager capacityMGer;
+    string onBoat;
+    string originalText;
+    void Start()
+    {
+        // modify text
+        onBoatDisplay = GameObject.Find("CustomersOnBoatText");
+        onBoatDisplayText = onBoatDisplay.GetComponent<Text>();
+        originalText = onBoatDisplayText.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        onBoat = (capacityMGer.customersOnBoat).ToString();
+        onBoatDisplayText.text = originalText + onBoat;
+    }
+}
