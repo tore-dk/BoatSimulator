a1504ef031973667cc3abddb20c00fffa2905d6f
Tore Friis <torefriis@gmail.com>
2020-07-06 11:48:54 +0200

    Camera movement!!!

--- Assets/Script/CameraMovement.cs
+++ Assets/Script/CameraMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Vector3 ResetCamera;
+    private Vector3 Origin;
+    private Vector3 Diference;
+    private bool Drag=false;
+    void Start () {
+        ResetCamera = Camera.main.transform.position;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void LateUpdate () {
+        if (Input.GetMouseButton (0)) {
+            Diference=(Camera.main.ScreenToWorldPoint (Input.mousePosition))- Camera.main.transform.position;
+        if (Drag==false){
+            Drag=true;
+            Origin=Camera.main.ScreenToWorldPoint (Input.mousePosition);
+        }
+        } else {
+            Drag=false;
+        }
+        if (Drag==true){
+            Camera.main.transform.position = Origin-Diference;
+        }
+        //RESET CAMERA TO STARTING POSITION WITH RIGHT CLICK
+        if (Input.GetMouseButton (1)) {
+            Camera.main.transform.position=ResetCamera;
+        }
     }
 }
